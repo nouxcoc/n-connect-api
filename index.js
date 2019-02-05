6de71a303cac58a5dada580ab328766cf55baf7d
Anil Prakash D'souza  (BLR GSS) <anilprakashs@nousinfo.com>
2019-02-05 16:42:57 +0530

    updated CORS Origin

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var projectsController = require('./controllers/projectsController');
 
 var app = express();
 app.use(bodyParser.json());
-app.use(cors({ origin: 'https://n-connect.herokuapp.com/' }));
+app.use(cors({ origin: '*' }));
 
 var server = app.listen(process.env.PORT || 3000, () => console.log('Server started at port : 3000'));
 var io = require('socket.io').listen(server);
--- user/userController.js
+++ user/userController.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
+var ObjectId = require('mongoose').Types.ObjectId;
 
 var VerifyToken = require('../auth/verifyToken');
 
@@ -10,10 +11,10 @@ var User = require('./user');
 // CREATES A NEW USER
 router.post('/', function (req, res) {
     User.create({
-            name : req.body.name,
-            email : req.body.email,
-            password : req.body.password
-        }, 
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    },
         function (err, user) {
             if (err) return res.status(500).send("There was a problem adding the information to the database.");
             res.status(200).send(user);
@@ -28,27 +29,45 @@ router.get('/', function (req, res) {
     });
 });
 
-// GETS A SINGLE USER FROM THE DATABASE
-router.get('/:id', function (req, res) {
-    User.findById(req.params.id, function (err, user) {
-        if (err) return res.status(500).send("There was a problem finding the user.");
-        if (!user) return res.status(404).send("No user found.");
-        res.status(200).send(user);
-    });
+// // GETS A SINGLE USER FROM THE DATABASE
+// router.get('/:id', function (req, res) {
+//     User.findById(req.params.id, function (err, user) {
+//         if (err) return res.status(500).send("There was a problem finding the user.");
+//         if (!user) return res.status(404).send("No user found.");
+//         res.status(200).send(user);
+//     });
+// });
+
+// GET : localhost:3000/todolist/userid
+router.get('/:user', (req, res) => {
+    console.log(req.params.user);
+    User.findOne({ name: req.params.user }, (err, doc) => {
+        if (!err) { 
+            if(doc){
+                res.send(doc.name);
+            }
+            else{
+                res.send(doc);
+            }
+         }
+        else {
+            console.log('Error in Retrieving ToDo List  : ' + JSON.stringify(err, undefined, 2));
+        }
+    })
 });
 
 // DELETES A USER FROM THE DATABASE
 router.delete('/:id', function (req, res) {
     User.findByIdAndRemove(req.params.id, function (err, user) {
         if (err) return res.status(500).send("There was a problem deleting the user.");
-        res.status(200).send("User: "+ user.name +" was deleted.");
+        res.status(200).send("User: " + user.name + " was deleted.");
     });
 });
 
 // UPDATES A SINGLE USER IN THE DATABASE
 // Added VerifyToken middleware to make sure only an authenticated user can put to this route
 router.put('/:id', /* VerifyToken, */ function (req, res) {
-    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
+    User.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, user) {
         if (err) return res.status(500).send("There was a problem updating the user.");
         res.status(200).send(user);
     });
